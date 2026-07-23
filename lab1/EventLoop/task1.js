38ae1bb7142f3c38189f731e684b88c37b0c58d4
Aryan Choudhary <aryan99chaudhary@gmail.com>
2026-07-23 15:24:23 +0530

    stack

--- lab1/EventLoop/task1.js
+++ lab1/EventLoop/task1.js
@@ -1,27 +1,41 @@
 import { log } from "console"
 
 const f1=() =>{
-    console.log("f1");
+    console.log("f1 starts");
+    f2();
+    console.log("f1 running");
+    console.log("f1 end");
+    
+    
     
 }
 const f2=() =>{
-    console.log("f2");
+    console.log("f2 starts");
+    f3();
+    console.log("f2 running");
+    console.log("f2 end");
+    
+    
     
 }
 const f3=()=>{
-    console.log("f3");
+    console.log("f3 starts");
+    console.log("f3 running");
+    console.log("f3 end");
+    
+    
+
     
 }
 
 function main(){
-    console.log("main");
+    console.log("main starts");
     f1();
-    f2();
-    f3();
-    console.log("exit main");
+    console.log("main running");
+    
+    console.log("main ends");
     
     
 }
 
 main();
-
