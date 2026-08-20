816e1999be5c2fbba2b5d8060de1ca2cdf755c23
Aryan Choudhary <aryan99chaudhary@gmail.com>
2026-08-20 14:50:57 +0530

    changes

--- _LaB3/prg1.js
+++ _LaB3/prg1.js
@@ -8,4 +8,5 @@ const server = http.createServer((req, res) => {
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    
 });
---
+++ _LaB3/prg2.js
@@ -0,0 +1,13 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.end('Hello, World!\n');
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+    res.statusCode = 200;
+    res.end(`
+        <h1>Hello, World!</h1>
+        <p>Server running on port ${PORT}</p>
+    `);
+});
