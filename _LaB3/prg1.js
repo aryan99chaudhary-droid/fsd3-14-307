bf0e9a0d5b1f97046fe7b983c793525c4ae22402
Aryan Choudhary <aryan99chaudhary@gmail.com>
2026-08-20 14:07:37 +0530

    new changes

---
+++ _LaB3/prg1.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!\n');
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
