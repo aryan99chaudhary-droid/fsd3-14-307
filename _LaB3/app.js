694d6b4292bdf9e0f59830af20fdccfd5f1b5876
Aryan Choudhary <aryan99chaudhary@gmail.com>
2026-09-08 10:13:09 +0530

    add in app.js

--- _LaB3/app.js
+++ _LaB3/app.js
@@ -1,16 +1,31 @@
 import http from 'http';
-import * as team from "./teams.js";
+import * as team from "./
+teams.js";
 
 const PORT = 5000;
-const server = http.createServer(req, res => {
-  if (req.url === '/' && req.method === 'GET') {
-    const teams = team.getAllTeams();
-    res.write(JSON.stringify(teams));
-  }else{
-    res.statusCode = 404;
-  }
-  res.end();
-});
+const sendJason = (res,statusCode,data) => {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(data === "undefined" ? "" : JSON.stringify(data));
+};
+
+const parseJSONBody = (req)=>{
+  new Promise((resolve, reject) => {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        const jsonData = JSON.parse(body);
+        resolve(jsonData);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+};
+
+const server = http.createServer((req, res) => {});
 server.listen(PORT, () => {
   console.log('Server is running');
 });
