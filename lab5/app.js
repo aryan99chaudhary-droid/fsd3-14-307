dd2f904ae7cb3837d61093d95012a0399a3f8f8f
Aryan Choudhary <aryan99chaudhary@gmail.com>
2026-09-17 13:57:42 +0530

    new commit

--- lab5/app.js
+++ lab5/app.js
@@ -9,3 +9,16 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is running at http://localhost:3000");
 });
+
+app.post('/login', (req, res) => {
+    
+    res.send('User Login');
+});
+
+app.put('/update', (req, res) => {
+    res.send('Update User');
+});
+
+app.delete('/delete', (req, res) => {
+    res.send('Delete User');
+});
